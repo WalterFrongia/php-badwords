11c5f760f9dcd8e0fc13e63ef3abd5cc7ee4b7d1
WalterFrongia <walter.frongia@libero.it>
2021-10-12 18:39:31 +0200

    Impostata repository e utilizzato php per esercizio

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+    $testo = "Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. Fu reso popolare, negli anni ’60, con la diffusione dei fogli di caratteri trasferibili “Letraset”, che contenevano passaggi del Lorem Ipsum, e più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum.";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Paragrafo</h1>
+    <p><?php echo $testo; ?> </p>
+    <p><?php echo $_GET["word"]?></p>
+    <p><?php echo str_replace ($_GET["word"], "***", $testo)?></p>
+
+</body>
+</html>
